a3fecac60f78a561f1481eb2f4d74351afceec6a
Joao Brambati <joaobrambati@gmail.com>
2026-02-20 10:53:13 -0300

    feat: implementa repositório de Livro e interface ILivroRepository

---
+++ backend/Infrastructure/Repositories/Implementations/LivroRepository.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Entities;
+using Infrastructure.Data;
+using Infrastructure.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories.Implementations;
+
+public class LivroRepository : ILivroRepository
+{
+    private readonly AppDbContext _context;
+
+    public LivroRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Livro>> GetAll()
+        => await _context.Livros.ToListAsync();
+
+    public async Task<Livro?> GetById(int id)
+        => await _context.Livros.FindAsync(id);
+
+    public async Task Create(Livro livro)
+        => await _context.Livros.AddAsync(livro);
+
+    public void Update(Livro livro)
+        => _context.Livros.Update(livro);
+
+    public void Delete(Livro livro)
+        => _context.Livros.Remove(livro);
+
+    public async Task SaveChangesAsync()
+        => await _context.SaveChangesAsync();
+
+}
---
+++ backend/Infrastructure/Repositories/Interfaces/ILivroRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Infrastructure.Repositories.Interfaces;
+
+public interface ILivroRepository
+{
+    Task<List<Livro>> GetAll();
+    Task<Livro?> GetById(int id);
+    Task Create(Livro livro);
+    void Update(Livro livro);
+    void Delete(Livro livro);
+    Task SaveChangesAsync();
+}
