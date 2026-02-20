120ffe8281f9319948b4f5aac23ac54712af4bb0
Joao Brambati <joaobrambati@gmail.com>
2026-02-20 19:06:07 -0300

    feat: adiciona métodos estáticos à classe Response<T> com status

--- backend/Application/Response/Response.cs
+++ backend/Application/Response/Response.cs
@@ -5,4 +5,21 @@ public class Response<T>
     public T? Data { get; set; }
     public bool Status { get; set; }
     public required string Message { get; set; }
+
+
+    public static Response<T> Success(T data, string message)
+    {
+        return new Response<T> { Data = data, Status = true, Message = message };
+    }
+
+    public static Response<T> Error(string message)
+    {
+        return new Response<T> { Status = false, Message = message };
+    }
+
+    public static Response<T> NotFound(string message)
+    {
+        return new Response<T> { Status = false, Message = message };
+    }
+
 }
